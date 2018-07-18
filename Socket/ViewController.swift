ec8addda079c643eae2464cace2744ae07d08dde
Zackory <zackorycramer@gmail.com>
2018-07-17 23:41:40 -0700

    Initial Commit

---
+++ Socket/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Socket
+//
+//  Created by Zackory Cramer on 7/17/18.
+//  Copyright © 2018 Aoil Applications. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
